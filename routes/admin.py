6b98477ae98abf488ffd0cfeeb3f592949c7c69e
Damnsure <bing900713@163.com>
2017-05-31 15:31:55 +0800

    用户和用户管理界面

--- routes/admin.py
+++ routes/admin.py
@@ -1,10 +1,11 @@
+from flask.blueprints import Blueprint
+from flask import current_app as app
 from routes import *
 from models.title import Title
 from models.user import User
 from models.auth import Auth
 from models.access import Access
-from flask.blueprints import Blueprint
-from flask import current_app as app
+
 
 import qiniu
 from config import key
@@ -14,9 +15,35 @@ q = qiniu.Auth(key.qiniu_access_key, key.qiniu_secret_key)
 main = Blueprint('admin', __name__)
 
 
-@main.route('/document/<user_uuid>')
+@main.route('/users')
 @admin_required
 def document(user_uuid):
-    cu = current_user()
-    u = User.find_one(uuid=user_uuid)
-    u.pi
+    u = current_user()
+    ms = User.all()
+    return render_template('admin/users.html', ms=ms, u=u)
+
+
+@main.route('users', method=['POST'])
+@admin_required
+def users_search():
+    u = current_user()
+    form = request.form()
+    ms = User.search_or(form)
+    return render_template('admin/users.html', ms=ms, u=u)
+
+
+@main.route('/user/<int:id>')
+@admin_required
+def user(id):
+    u = current_user()
+    m = User.get(id)
+    return render_template('admin/user.html', m=m, u=u)
+
+
+@main.route('/user/update/<int:id>', methods=['POST'])
+@admin_required
+def user_update(id):
+    m = User.get(id)
+    form = request.form
+    m.update_user(form)
+    return redirect(url_for('admin.user', id=m.id))
